573bdc53cdd473f339d66aac23cfe528fa8d6c95
Samarth <samartha186@gmail.com>
2022-07-29 00:05:11 +0530

    Updates

---
+++ precache-manifest.3b49bce2e147dc142fd1607c6d65a9b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ef5fa1e9487c36530183",
+    "url": "/MyNewsApp-using-MERN/static/css/main.b935c391.chunk.css"
+  },
+  {
+    "revision": "ef5fa1e9487c36530183",
+    "url": "/MyNewsApp-using-MERN/static/js/main.ef5fa1e9.chunk.js"
+  },
+  {
+    "revision": "206e060bd7d3afd2158d",
+    "url": "/MyNewsApp-using-MERN/static/js/1.206e060b.chunk.js"
+  },
+  {
+    "revision": "027fc69fd394fc5f1dd8",
+    "url": "/MyNewsApp-using-MERN/static/js/2.027fc69f.chunk.js"
+  },
+  {
+    "revision": "3122b8a9bda71396bd28",
+    "url": "/MyNewsApp-using-MERN/static/js/runtime~main.3122b8a9.js"
+  },
+  {
+    "revision": "c2f34c8113afc07c48d9eb6aeb542c60",
+    "url": "/MyNewsApp-using-MERN/index.html"
+  }
+];
